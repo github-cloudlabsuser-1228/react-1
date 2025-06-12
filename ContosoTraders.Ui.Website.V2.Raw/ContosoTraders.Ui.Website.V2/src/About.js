cea21c9693e22a87b93f7e340f39f56cdad2c65d
unknown <github_cloudlabsuser_1228@cloudlabsaiuser.com>
2025-06-12 14:13:52 +0000

    Components added nav not working

---
+++ ContosoTraders.Ui.Website.V2.Raw/ContosoTraders.Ui.Website.V2/src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>About Us</h1>
+            <p>
+                Welcome to Contoso Traders! We are dedicated to providing the best products and services to our valued customers.
+            </p>
+            <p>
+                Our team is passionate about innovation and excellence. With years of experience in the industry, we strive to exceed your expectations every day.
+            </p>
+            <p>
+                Thank you for choosing Contoso Traders. We look forward to serving you!
+            </p>
+        </div>
+    );
+};
+
+export default About;
